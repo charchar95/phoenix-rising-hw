89b1f6658107482b94e4d21cb4c7407f5c16c122
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-02-16 14:14:11 -0700

    show views show req.params.id

--- w09d3_homework_pokeexpress/pokemon_app/server.js
+++ w09d3_homework_pokeexpress/pokemon_app/server.js
@@ -24,6 +24,11 @@ app.get('/pokemon', (req, res) => {
     });
 });
 
+app.get('/pokemon/:id', (req, res) => {
+    res.send(req.params.id);
+  }); 
+
+
 
 // =======================================
 //              LISTENER
