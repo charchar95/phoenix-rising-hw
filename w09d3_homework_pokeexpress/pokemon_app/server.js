a8d7d3eba44e4f2628da44d5fb4d8a7097932bd4
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-02-16 14:39:23 -0700

    created show views of each pokemon

--- w09d3_homework_pokeexpress/pokemon_app/server.js
+++ w09d3_homework_pokeexpress/pokemon_app/server.js
@@ -25,8 +25,12 @@ app.get('/pokemon', (req, res) => {
 });
 
 app.get('/pokemon/:id', (req, res) => {
-    res.send(req.params.id);
+    res.render('show.ejs', {
+        pokemon: pokemon[req.params.id]
+    });
   }); 
+
+
 
 
 
