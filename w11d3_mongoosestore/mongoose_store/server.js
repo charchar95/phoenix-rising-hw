4b4ea213478acf45ada392dc460241f60bfdd2e0
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-03-01 12:20:19 -0700

    index view added photos

--- w11d3_mongoosestore/mongoose_store/server.js
+++ w11d3_mongoosestore/mongoose_store/server.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
+const router = express.Router()
 
 // =======================================
 //              MIDDLEWARE
@@ -26,7 +27,7 @@ mongoose.connection.once('open', ()=> {
 // =======================================
 //              DATABASE   
 // ======================================= 
-const Products = require('./models/products.js')
+const Product = require('./models/products.js')
 
 // =======================================
 //              ROUTES
@@ -34,9 +35,9 @@ const Products = require('./models/products.js')
 
 // INDEX //
 app.get('/products', (req, res) => {
-    Fruit.find({}, (error, fruits)=>{
+    Product.find({}, (error, Product)=>{
         res.render('index.ejs', {
-            products: products
+            Product: Product
         });
     });
 });
@@ -100,9 +101,39 @@ router.delete("/:id", (req, res) => {
     });
   });
 
+ // SEED ROUTE //
+ app.get('/seed', (req, res) => {
+  Product.create(
+    [
+      {
+        name: 'Beans',
+        description: 'A small pile of beans. Buy more beans for a big pile of beans.',
+        img: 'https://cdn3.bigcommerce.com/s-a6pgxdjc7w/products/1075/images/967/416130__50605.1467418920.1280.1280.jpg?c=2',
+        price: 5,
+        qty: 99
+      }, {
+        name: 'Bones',
+        description: 'It\'s just a bag of bones.',
+        img: 'http://bluelips.com/prod_images_large/bones1.jpg',
+        price: 25,
+        qty: 0
+      }, {
+        name: 'Bins',
+        description: 'A stack of colorful bins for your beans and bones.',
+        img: 'http://www.clipartbest.com/cliparts/9cz/rMM/9czrMMBcE.jpeg',
+        price: 7000,
+        qty: 1
+      }
+    ],(err, data)=>{
+      console.log(data);
+      res.redirect('/products');
+    })
+})
+
 // =======================================
 //              LISTENER
 // =======================================
 app.listen(port, () => {
     console.log("Mongoose store listening")
 });
+
