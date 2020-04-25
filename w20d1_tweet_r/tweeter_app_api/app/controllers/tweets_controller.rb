c01644f9ce9ef46717695b1b4dd137efea43d7a7
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-04-25 11:59:50 -0700

    can POST new tweets

--- w20d1_tweet_r/tweeter_app_api/app/controllers/tweets_controller.rb
+++ w20d1_tweet_r/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -8,4 +8,19 @@ class TweetsController < ApplicationController
         tweet = Tweet.find(params[:id])
         render json: {status: 200, tweet: tweet}
     end
+
+    def create 
+        tweet = Tweet.new(tweet_params)
+        if tweet.save
+            render(status: 201, json:{tweet: tweet})
+        else 
+            render(status: 422, json:{tweet: tweet})
+        end
+    end
+
+    private
+    def tweet_params
+        # Returns a sanitized hash of the params with nothing extra
+        params.required(:tweet).permit(:title, :content, :author)
+    end
 end
