240721e04a14f846e043a37db1399ef3f91b4a8d
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-05-04 10:35:47 -0700

    react module and controller

--- w20d3_datingapp/dating_app_client/src/components/hello.js
+++ w20d3_datingapp/dating_app_client/src/components/hello.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 
 class Daters extends Component {
+    getDater () {
+        fetch('http://localhost:3000/daters')
+        .then( daters => console.log(daters))
+    }
     render () {
         return (
             <h1>Hello Dating World</h1>
