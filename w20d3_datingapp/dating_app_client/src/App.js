c61571cc8668c01b0bb3bec506b70dc74b85a4f5
Charlie Humphrey <charliehumphrey@Charlies-MacBook-Pro.local>
2020-05-04 10:32:57 -0700

    made react app

---
+++ w20d3_datingapp/dating_app_client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Daters from '.components/hello.js'
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App"> 
+        <Daters />
+      </div>
+    )
+  }
+}
+export default App;
